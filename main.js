00ef25a4c02d3b2573342f3fa3338536afa5dfd1
Adrian Szlegel <adrian.szlegel@hotmail.com>
2024-02-13 23:44:32 +0100

    blockchain class added

--- main.js
+++ main.js
@@ -13,3 +13,23 @@ class Block {
     return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
   }
 }
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() { // first block of a blockchain
+    return new Block(0, "01/01/2024", "Genesis Block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+}
