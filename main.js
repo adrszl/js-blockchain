173ce667fcbd28d20370f2af10f04e35703c4b63
Adrian Szlegel <adrian.szlegel@hotmail.com>
2024-07-01 22:40:57 +0200

    new class Transaction added & changes in Block class

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 const SHA256 = require('crypto-js/sha256');
 
+class Transaction {
+  constructor(fromAddress, toAddress, amount) {
+    this.fromAddress = fromAddress;
+    this.toAddress = toAddress;
+    this.amount = amount;
+  }
+}
+
 class Block {
-  constructor(index, timestamp, data, previousHash = '') {
-    this.index = index;
+  constructor(index, timestamp, transactions, previousHash = '') {
     this.timestamp = timestamp;
-    this.data = data;
+    this.transactions = transactions;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
     this.nonce = 0;
   }
 
   calculateHash() {
-    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
   }
 
   mineBlock(difficulty) {
@@ -33,7 +40,7 @@ class Blockchain {
   }
 
   createGenesisBlock() { // first block of a blockchain
-    return new Block(0, "01/01/2024", "Genesis Block", "0");
+    return new Block("01/01/2024", "Genesis Block", "0");
   }
 
   getLatestBlock() {
